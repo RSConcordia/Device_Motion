8ba2c1a2e60216a52ef36286fe4d0d3273cd054d
RSConcordia <renansconcordia@gmail.com>
2015-11-11 15:18:43 -0200

    Device Motion

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-/*var app = {			
+var app = {			
 	
 	};
-	var inicial = 0;
 	var contador = 0;	
+	var verificador = 0;
 	
 	function accelerometer(){ 
+		document.getElementById('status').innerHTML = "";
 		navigator.accelerometer.getCurrentAcceleration(motion, error);
 	}
 	function motion(acceleration){
@@ -17,108 +18,36 @@
 		var accel_z = acceleration.z;
 		var accel_z = parseInt(accel_z); 
 		
-		if(inicial == 0){
-			localStorage.setItem("x", accel_x);	
-			localStorage.setItem("y", accel_y);	
-			localStorage.setItem("z", accel_z);	
-			inicial = 1;
-			setTimeout(accelerometer, 500);
-		}
-		else 
-		{		
-			var x =	localStorage.getItem("x");
-			var y =	localStorage.getItem("y");
-			var z =	localStorage.getItem("z");
-			
-			if(accel_x != x){
-				if(accel_y != y || accel_z != z){
-					start();
-				}
-				else
-				{
-					document.getElementById('status').innerHTML = inicial;
-					inicial++;
-					setTimeout(accelerometer, 500);	
-				}
+		if(contador < 5){
+				localStorage.setItem("accel_x"+contador, accel_x);
+				localStorage.setItem("accel_y"+contador, accel_y);
+				localStorage.setItem("accel_z"+contador, accel_z);
+				
+				document.getElementById('status').innerHTML += "<br>contador "+contador+"<br>X - "+accel_x+"<br>Y - "+accel_y+"<br>Z - "+accel_z;
+				
+				contador++;
+				accelerometer();
+			} 
+			else
+			{
+				verificacao();
 			}
-			if(accel_y != y){
-				if(accel_x != x || accel_z != z){
-					start();
-				}
-				else
-				{
-					document.getElementById('status').innerHTML = inicial;
-					inicial++;
-					setTimeout(accelerometer, 500);	
-				}
-			}			
-			if(accel_z != z){
-				if(accel_y != y || accel_x != x){
-					start();
-				}
-				else
-				{
-					document.getElementById('status').innerHTML = inicial;
-					inicial++;
-					setTimeout(accelerometer, 500);	
-				}
-			}
-		}
-	}
-	function start(){
-		navigator.accelerometer.getCurrentAcceleration(motion_accel, error);		
-	}
-	function motion_accel(acceleration){
-		var accel_x = acceleration.x;
-		var x = parseInt(accel_x); 
 		
-		var accel_y = acceleration.y;
-		var y =parseInt(accel_y); 
-		
-		var accel_z = acceleration.z;
-		var z = parseInt(accel_z); 
-		
-		if(contador < 5){			
-			document.getElementById('status').innerHTML += "<br>"+contador+"<br>X "+x+"<br>Y "+y+"<br>Z "+z+"<br>------------<br>";
-			contador++;
-			setTimeout(start, 3000);
-		}
-		else
-		{		
-			document.getElementById('status').innerHTML += "<br>...";
-		}
 	}
 	function error(){
 		alert('Error!');
 	}
-	
-	*/
-	function accelerometer(){ 
-		navigator.accelerometer.getCurrentAcceleration(motion, error);
-	}	
-	var inicial = 0;
-	var contador = 0;
-	function motion(acceleration){
-		var accel_x = acceleration.x;
-		var accel_x = parseInt(accel_x); 
+	function verificacao(){
+		for (primeiro = 0; primeiro < 4; primeiro++){
+				var segundo = primeiro+1; 
+				if (localStorage.getItem("string"+primeiro) != localStorage.getItem("string"+segundo)){	
+					verificador++;
+				}
+			}
+		document.getElementById('status').innerHTML += "<br>Ocorreram "+verificador+" mudan?as";
 		
-		var accel_y = acceleration.y;
-		var accel_y =parseInt(accel_y); 
-		
-		var accel_z = acceleration.z;
-		var accel_z = parseInt(accel_z); 
+		contador = 0;	
+		verificador = 0;
+	}
 	
-		if(contador < 5){			
-			document.getElementById('status').innerHTML += "<br>"+contador+"<br>X "+accel_x+"<br>Y "+accel_y+"<br>Z "+accel_z+"<br>------------<br>";
-			contador++;
-			setTimeout(accelerometer, 800);
-		}
-		else
-		{		
-			document.getElementById('status').innerHTML += "<br>...";
-		}
-	}
-	function error(){
-		alert('Error!');
-	}
 	
