a7fc89fbb63cb18804ad2e05e0004cfce9e3b4d1
RSConcordia <renansconcordia@gmail.com>
2015-11-26 16:07:09 -0200

    Motion

--- js/index.js
+++ js/index.js
@@ -66,13 +66,13 @@ var app = {
 		media = soma_eixo / 50;
 		media = parseInt(media);
 		document.getElementById("status").innerHTML += "<br>Media dos Eixos "+ media+"<br>"; 
+		document.getElementById("status").innerHTML += "<h2>Vetores</h2><br>"; 
 		
 		for (i = 0; i < 50; i++){	
-			var eixo = localStorage.getItem("eixo");
+			var eixo = localStorage.getItem("eixo"+i);
 			v = eixo - media;
 			vetor = parseInt(v);
 			
-			document.getElementById("status").innerHTML += "<h2>Vetores</h2><br>"; 
 			document.getElementById("status").innerHTML +=  vetor+"/"; 
 			
 			if(!anterior){
