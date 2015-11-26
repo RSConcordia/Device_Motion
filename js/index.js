1c89f60071df453a239c3799f647236398c31d37
RSConcordia <renansconcordia@gmail.com>
2015-11-26 14:06:02 -0200

    Motion

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ var app = {
 	};
 	var loop = 0;	
 	var contador = 0;
-	var soma_media = 0;
+	var soma_eixo = 0;
+	var eixo = 0;
 	var anterior = false;
-	var frequencia = 0;
 	
 	
 	function start(){ 
@@ -14,7 +14,6 @@ var app = {
 		loop = 0;	
 		contador = 0;
 		soma_media = 0;
-		frequencia = 0;
 		anterior = false;
 		
 		accelerometer();
@@ -33,16 +32,19 @@ var app = {
 		var accel_z = parseInt(accel_z); 
 		
 		if(loop < 50){			
-			var soma_eixo = accel_x + accel_y + accel_z;
-			soma_media = soma_media + soma_eixo;
-			
-			localStorage.setItem("soma_eixo_"+loop, soma_eixo);
+			document.getElementById("status").innerHTML += "<br><h2>"+loop+"</h2>"; 		
+			var eixo = accel_x + accel_y + accel_z;
+			document.getElementById("status").innerHTML += "<br>Eixo "+ eixo; 		
+			soma_eixo = soma_eixo + eixo;
+				
+			localStorage.setItem("soma_eixo_"+loop, eixo);
 			
 			loop++;
 			setTimeout(accelerometer, 100);	
 		}
 		else
 		{			
+			document.getElementById("status").innerHTML += "<br>Soma de Todos os Eixos "+ soma_eixo; 
 			vetor();
 		}
 	}
@@ -50,13 +52,16 @@ var app = {
 		alert('Error!');
 	}
 	function vetor(){
-		var media = soma_media / 50;
+		var media = soma_eixo / 50;
+		var media = parseInt(media);
+		document.getElementById("status").innerHTML += "<br>Media dos Eixos "+ media; 
 		for (i = 0; i < 50; i++){				
 			var eixo = localStorage.getItem("soma_eixo_"+i);
-			var vetor = eixo - media;		
+			var vetor = eixo - media;
+			var vetor = parseInt(vetor);
 		
 			if(!anterior){
-				if(vetor > 0){
+				if(vetor > "0"){
 					contador++;
 					anterior = true;					
 				}
@@ -67,7 +72,9 @@ var app = {
 			}
 			else 
 			{
-				if(vetor < 0) { anterior = false; }
+				if(vetor < "0") {
+					anterior = false; 
+				}
 			}  
 		} 
 		
